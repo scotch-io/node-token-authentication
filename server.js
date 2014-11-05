899e2301e202ef8d8da7ab297b49ef088ba67faf
Chris Sevilleja <chris@scotch.io>
2014-11-04 22:27:01 -0800

    doing things

--- server.js
+++ server.js
@@ -9,4 +9,8 @@ var express 	= require('express'),
 // set the port
 var port = process.env.PORT || 8080;
 
-// configure
+// configure app to use json
+app.use(express.json());
+
+// configure app to urlencode
+app.use(express.urlencoded());
