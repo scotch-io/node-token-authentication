58e04cce22514580733c698fe5372abc24661dd0
Chris Sevilleja <chris@scotch.io>
2014-11-11 15:47:11 -0800

    whyyy

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 // get the packages we need ========================================
-var express 	= require('express'),
-	app         = express(),
-	expressJWT  = reqiure('express-jwt'),
-	jwt         = require('jsonwebtoken');
+var express 	= require('express');
+var app         = express();
+
+// used to create, sign, and verify tokens
+var jwt         = require('jsonwebtoken');
 
 // configuration ===================================================
-
 // set the port
 var port = process.env.PORT || 8080;
 
-// configure app to use json
-app.use(express.json());
+// routes ==========================================================
 
-// configure app to urlencode
-app.use(express.urlencoded());
+var token = jwt.sign({ user: 'chris' }, 'ilovescotchyscotch');
+jwt.verify(token, 'ilovescotchyscotch', function(err, decoded) {
+	console.log(decoded);
+	var verified = decoded;
+});
+
+app.get('/', function(req, res) {
+	res.send(token);
+});
+
+app.get('/check', function(req, res) {
+	console.log(decoded);
+	res.send('hello');
+});
+
+// start the server
+app.listen(port);
