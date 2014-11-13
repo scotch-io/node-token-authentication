e4ec1f13f79815edfcd4c4e94b82873ddbb351af
Chris Sevilleja <chris@scotch.io>
2014-11-12 16:01:27 -0800

    more work

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // get the packages we need ========================================
 var express 	= require('express');
 var app         = express();
+var bodyParser  = require('body-parser');
 
 // used to create, sign, and verify tokens
 var jwt         = require('jsonwebtoken');
@@ -9,22 +10,64 @@ var jwt         = require('jsonwebtoken');
 // set the port
 var port = process.env.PORT || 8080;
 
-// routes ==========================================================
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-var token = jwt.sign({ user: 'chris' }, 'ilovescotchyscotch');
-jwt.verify(token, 'ilovescotchyscotch', function(err, decoded) {
-	console.log(decoded);
-	var verified = decoded;
+// routes ==========================================================
+var info = {
+	user: 'Chris Sevilleja',
+	admin: true
+};
+
+var token = jwt.sign(info, 'ilovescotchyscotch', {
+	issuer: 1, // user id
+	expiresInMinutes: 1440 // expires in 24 hours
 });
 
-app.get('/', function(req, res) {
+// get an instance of the router
+var apiRoutes = express.Router();
+
+// authentication
+apiRoutes.post('/authenticate', function(req, res) {
+	console.log(req.params);
+});
+
+apiRoutes.use(function(req, res, next) {
+
+	// check header or url parameters or post parameters for token
+	var token = (req.body && req.body.token) || (req.query && req.query.token) || req.headers['x-access-token'];
+
+	// decode token
+	if (token) {
+
+		// verifies secret and checks exp
+		jwt.verify(token, 'ilovescotchyscotch', function(err, decoded) {
+			console.log(decoded);
+
+			// if everything is good, save to request for use in other routes
+			req.decoded = decoded;
+
+			if (err) {
+				res.end('error', 400);
+			}
+		});
+
+	}
+
+	next();
+});
+
+apiRoutes.get('/', function(req, res) {
 	res.send(token);
 });
 
-app.get('/check', function(req, res) {
-	console.log(decoded);
+apiRoutes.get('/check', function(req, res) {
 	res.send('hello');
+
 });
+
+app.use('/api', apiRoutes);
 
 // start the server
 app.listen(port);
