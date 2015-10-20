a7b60e8498ffe1c179a545009945d8974a9168c4
abr4xas <me@abr4xas.org>
2015-10-20 03:32:03 -0430

    actualizando aplicacion :bug:

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 // =================================================================
 // get the packages we need ========================================
 // =================================================================
-var express 	= require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
-var morgan      = require('morgan');
-var mongoose    = require('mongoose');
-
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
-var User   = require('./app/models/user'); // get our mongoose model
+var express 	= require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    morgan      = require('morgan'),
+    mongoose    = require('mongoose'),
+    jwt    = require('jsonwebtoken'),
+    config = require('./config'),
+    User   = require('./app/models/user'); // get our mongoose model
 
 // =================================================================
 // configuration ===================================================
@@ -30,29 +29,29 @@ app.use(morgan('dev'));
 // =================================================================
 app.get('/setup', function(req, res) {
 
-	// create a sample user
-	var nick = new User({ 
-		name: 'Nick Cerminara', 
-		password: 'password',
-		admin: true 
-	});
-	nick.save(function(err) {
-		if (err) throw err;
+    // create a sample user
+    var nick = new User({
+        name: 'abr4xas',
+        password: '123456',
+        admin: true
+    });
+    nick.save(function(err) {
+        if (err) throw err;
 
-		console.log('User saved successfully');
-		res.json({ success: true });
-	});
+        console.log('User saved successfully');
+        res.json({ success: true });
+    });
 });
 
 // basic route (http://localhost:8080)
 app.get('/', function(req, res) {
-	res.send('Hello! The API is at http://localhost:' + port + '/api');
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
 // ---------------------------------------------------------
 // get an instance of the router for api routes
 // ---------------------------------------------------------
-var apiRoutes = express.Router(); 
+var apiRoutes = express.Router();
 
 // ---------------------------------------------------------
 // authentication (no middleware necessary since this isnt authenticated)
@@ -60,38 +59,38 @@ var apiRoutes = express.Router();
 // http://localhost:8080/api/authenticate
 apiRoutes.post('/authenticate', function(req, res) {
 
-	// find the user
-	User.findOne({
-		name: req.body.name
-	}, function(err, user) {
+    // find the user
+    User.findOne({
+        name: req.body.name
+    }, function(err, user) {
 
-		if (err) throw err;
+        if (err) throw err;
 
-		if (!user) {
-			res.json({ success: false, message: 'Authentication failed. User not found.' });
-		} else if (user) {
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
 
-			// check if password matches
-			if (user.password != req.body.password) {
-				res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-			} else {
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
 
-				// if user is found and password is right
-				// create a token
-				var token = jwt.sign(user, app.get('superSecret'), {
-					expiresInMinutes: 1440 // expires in 24 hours
-				});
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign(user, app.get('superSecret'), {
+                    expiresInMinutes: 1440 // expires in 24 hours
+                });
 
-				res.json({
-					success: true,
-					message: 'Enjoy your token!',
-					token: token
-				});
-			}		
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
 
-		}
+        }
 
-	});
+    });
 });
 
 // ---------------------------------------------------------
@@ -99,51 +98,51 @@ apiRoutes.post('/authenticate', function(req, res) {
 // ---------------------------------------------------------
 apiRoutes.use(function(req, res, next) {
 
-	// check header or url parameters or post parameters for token
-	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+    // check header or url parameters or post parameters for token
+    var token = req.headers['x-access-token'];
 
-	// decode token
-	if (token) {
+    // decode token
+    if (token) {
 
-		// verifies secret and checks exp
-		jwt.verify(token, app.get('superSecret'), function(err, decoded) {			
-			if (err) {
-				return res.json({ success: false, message: 'Failed to authenticate token.' });		
-			} else {
-				// if everything is good, save to request for use in other routes
-				req.decoded = decoded;	
-				next();
-			}
-		});
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
 
-	} else {
+    } else {
 
-		// if there is no token
-		// return an error
-		return res.status(403).send({ 
-			success: false, 
-			message: 'No token provided.'
-		});
-		
-	}
-	
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+
 });
 
 // ---------------------------------------------------------
 // authenticated routes
 // ---------------------------------------------------------
 apiRoutes.get('/', function(req, res) {
-	res.json({ message: 'Welcome to the coolest API on earth!' });
+    res.json({ message: 'Welcome to the coolest API on earth!' });
 });
 
 apiRoutes.get('/users', function(req, res) {
-	User.find({}, function(err, users) {
-		res.json(users);
-	});
+    User.find({}, function(err, users) {
+        res.json(users);
+    });
 });
 
 apiRoutes.get('/check', function(req, res) {
-	res.json(req.decoded);
+    res.json(req.decoded);
 });
 
 app.use('/api', apiRoutes);
